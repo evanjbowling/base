43f9011bb342d61045c56f71d50111c6c0bf8267
Evan Bowling <evan.j.bowling@gmail.com>
2019-02-27 00:34:25 -0600

    Refactor conversion logic as lazy seq
    - adds mapping logic as an optional parameter
    - adds additional example base alphabet encodings
      with RFC references
    - still need to assert that ::digit-mappings has a
      length equal to base

--- src/com/evanjbowling/base.clj
+++ src/com/evanjbowling/base.clj
@@ -1,18 +1,32 @@
 (ns com.evanjbowling.base
   (:require
-   [clojure.math.numeric-tower :as math]))
+   [clojure.math.numeric-tower :as math]
+   [clojure.string             :as string])
+  (:import
+   (java.math BigDecimal)))
 
-(defn ^:private parts
-  "Split a decimal into integer and fraction parts"
+(defn ^:private split'
+  "Split decimal into integral and fractional parts.
+  Support arbitrary precision."
   [^BigDecimal d]
   (let [i (.divideToIntegralValue d 1M)]
-    [(bigint i) (-' d i)]))
+    {::integer (bigint i)
+     ::fraction (-' d i)}))
+
+(defn ^:private split
+  "Split decimal into integral and fractional parts.
+  Will throw if integer portion is larger than a long
+  can hold."
+  [^BigDecimal d]
+  (let [i (.divideToIntegralValue d 1M)]
+    {::integer (.longValueExact i)
+     ::fraction (-' d i)}))
 
 (defn ^:private max-digit
-  [i base idx]
+  [i base e]
   (loop [d 1]
-    (if (and (<= (*' d (math/expt base idx)) i)
-             (< i (*' (inc d) (math/expt base idx))))
+    (if (and (<= (*' d (math/expt base e)) i)
+             (< i (*' (inc d) (math/expt base e))))
       d
       (recur (inc d)))))
 
@@ -20,22 +34,14 @@
   [i base]
   (if (<= i (dec base))
     {0 i}
-    (loop [k 1
+    (loop [e 1
            prev-max (dec base)]
-      (let [max-val-w-curr-k (+' prev-max
+      (let [max-val-w-curr-e (+' prev-max
                                  (*' (dec base)
-                                     (math/expt base k)))]
-        (if (<= i max-val-w-curr-k)
-          {k (max-digit i base k)}
-          (recur (inc k) max-val-w-curr-k))))))
-
-(defn ^:private map-digit
-  [d]
-  (if (< d 10)
-    d
-    (get {10 \a, 11 \b, 12 \c
-          13 \d, 14 \e, 15 \f}
-         d)))
+                                     (math/expt base e)))]
+        (if (<= i max-val-w-curr-e)
+          {e (max-digit i base e)}
+          (recur (inc e) max-val-w-curr-e))))))
 
 (defn ^:private reduce-digits
   [digits]
@@ -46,20 +52,18 @@
                               (apply max)
                               inc
                               range))
-         result ""]
+         result []]
     (if (empty? digit-indices)
       result
-      (let [unmapped-digit
+      (let [digit
             (if-let [d (get digits (first digit-indices))]
               d
               0)]
-        (recur (rest digit-indices)
-               (str (map-digit unmapped-digit) result))))))
+        (recur (rest digit-indices) (cons digit result))))))
 
 (defn ^:private int-to-base
   "Convert integer portion to other base."
   [i base]
-  {:pre (< 1 base)}
   (loop [left i
          digits {}]
     (if (= 0 left)
@@ -70,28 +74,87 @@
                (merge digits next-digit))))))
 
 (defn ^:private fraction-to-base
-  "Convert fractional portion d >= 0 and d < 1
-   to other base. First form extracts 10 digits.
-  Second form extracts n digits."
-  ([d base]
-   (fraction-to-base d base 10))
-  ([d base num-digits]
-   (loop [n 0
-          result (if (= 0M d) "0" "")
-          left d]
-     (if (or (= 0M left) (= n num-digits))
-       {:value result, :remainder left}
-       (let [[i f] (parts (*' base left))]
-         (recur (inc n) (str result (map-digit i)) f))))))
+  "Returns a lazy sequence of the fractional values where
+  each v >= 0 and < base. Note that this may be an infinite
+  sequence."
+  ([^BigDecimal d base]
+   (lazy-seq
+    (if (= 0M d)
+      [0]
+      (let [{i ::integer, f ::fraction} (split (*' base d))]
+        (cons i (if (= 0M f)
+                  []
+                  (fraction-to-base f base))))))))
+
+(defn ^:private map-fraction
+  "Map up to max digits of the fractional value sequence."
+  [max mapping s]
+  (->> (take max s)
+       (map (partial nth mapping))))
+
+(defn ^:private map-integer
+  "Map digits of the integer value sequence."
+  [mapping s]
+  (map (partial nth mapping) s))
 
 (defn to-base
   "Convert decimal value into another base
   representation."
-  [d base]
-  (let [d (bigdec d)
-        base (long base)
-        [i f] (parts d)]
-    (format "%s.%s"
-            (int-to-base i base)
-            (:value (fraction-to-base f base)))))
+  ([d base]
+   (to-base d base {}))
+  ([d base opts]
+   (let [ms (or (::max-scale opts) 10)
+         ds (or (::decimal-separator opts) \.)
+         dm (or (::digit-mapping opts) "0123456789abcdef")
+         d (bigdec d)
+         base (long base)
+         {i ::integer, f ::fraction} (split' d)]
+     (string/join ""
+                  (concat (map-integer dm (int-to-base i base))
+                          [ds]
+                          (map-fraction ms dm (fraction-to-base f base)))))))
 
+;;
+;; convenience fns
+;;
+
+(def O-9 "0123456789")
+(def a-z "abcdefghijklmnopqrstuvwxyz")
+(def A-Z "ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+(def base2 "01")
+(def base8 "01234567")
+(def base16 (str O-9 "ABCDEF"))
+(def base32 (str A-Z "234567"))
+(def base32hex (subs (str O-9 A-Z) 0 32))
+(def base36 (str O-9 A-Z))
+(def base58 "No 0,I,O,l" "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz")
+(def base64 (str A-Z a-z O-9 "+/"))
+(def base64url (str A-Z a-z O-9 "-_"))
+
+(defn to-base2
+  "Convert decimal to binary string representation.
+  Second form takes options map. See to-base for details."
+  ([d] (to-base2 d {}))
+  ([d opts] (to-base d 2 (assoc opts ::digit-mapping base2))))
+
+(defn to-base8
+  "Convert decimal to octal string representation.
+  Second form takes options map. See to-base for details."
+  ([d] (to-base8 d {}))
+  ([d opts] (to-base d 2 (assoc opts ::digit-mapping base8))))
+
+(defn to-base16
+  "Convert decimal to hexadecimal string representation.
+  Second form takes options map. See to-base for details."
+  ([d] (to-base16 d {}))
+  ([d opts] (to-base d 16 (assoc opts ::digit-mapping base16))))
+
+(defn to-base32
+  "Convert decimal to base 32 string representation.
+
+  See https://tools.ietf.org/html/rfc4648#page-8
+  While RFC details encoding process for binary data,
+  only the alphabet is used here."
+  ([d] (to-base32 d {}))
+  ([d opts] (to-base d 32 (assoc opts ::digit-mapping base32))))
--- test/com/evanjbowling/base_test.clj
+++ test/com/evanjbowling/base_test.clj
@@ -26,25 +26,22 @@
     (is (= {15 8} (#'b/int-to-base* (+' Long/MAX_VALUE 1) 16)))))
 
 (deftest test-int-to-base
-  (is (= "0" (#'b/int-to-base 0 16)))
-  (is (= "b" (#'b/int-to-base 11 16)))
-  (is (= "f" (#'b/int-to-base 15 16)))
-  (is (= "10" (#'b/int-to-base 16 16)))
-  (is (= "102" (#'b/int-to-base 258 16)))
+  (is (= [0] (#'b/int-to-base 0 16)))
+  (is (= [11] (#'b/int-to-base 11 16)))
+  (is (= [15] (#'b/int-to-base 15 16)))
+  (is (= [1 0] (#'b/int-to-base 16 16)))
+  (is (= [1 0 2] (#'b/int-to-base 258 16)))
   #_(testing "base less than 2 throws"
       (is (thrown? AssertionError (#'b/int-to-base 13 -1)))
       (is (thrown? AssertionError (#'b/int-to-base 13 0)))
       (is (thrown? AssertionError (#'b/int-to-base 13 1)))))
 
 (deftest test-fraction-to-base
-  (is (= {:value "0"
-          :remainder 0M} (#'b/fraction-to-base 0.0M 2)))
-  (is (= {:value "01"
-          :remainder 0M} (#'b/fraction-to-base 0.25M 2)))
-  (is (= {:value "4"
-          :remainder 0M} (#'b/fraction-to-base 0.25M 16)))
-  (is (= {:value "1999999999"
-          :remainder 0.6M} (#'b/fraction-to-base 0.1M 16))))
+  (is (= [0] (take 10 (#'b/fraction-to-base 0.0M 2))))
+  (is (= [1] (take 10 (#'b/fraction-to-base 0.5M 2))))
+  (is (= [0 1] (take 10 (#'b/fraction-to-base 0.25M 2))))
+  (is (= [4] (take 10 (#'b/fraction-to-base 0.25M 16))))
+  (is (= [1 9 9 9 9 9 9 9 9 9] (take 10 (#'b/fraction-to-base 0.1M 16)))))
 
 (deftest test-to-base
   (testing "base 2"
@@ -52,42 +49,44 @@
            (b/to-base 1048576M 2)))))
 
 (deftest test-input-handling
-  (are [d base expected] (= expected (b/to-base d base))
-    1.25 16 "1.4"
-    1.25 16M "1.4"
-    1.25M 16M "1.4"
-    (int 12) 16 "c.0"
-    (long 12) 16 "c.0"
-    (short 12) 16 "c.0"))
+  (let [opts {::b/digit-mappings "0123456789abcdef"}]
+    (are [d base expected] (= expected (b/to-base d base opts))
+      1.25 16 "1.4"
+      1.25 16M "1.4"
+      1.25M 16M "1.4"
+      (int 12) 16 "c.0"
+      (long 12) 16 "c.0"
+      (short 12) 16 "c.0")))
 
 (deftest test-to-base-hand-verified
-  (are [d base expected] (= expected (b/to-base d base))
-    1.25M 2  "1.01"
-    1.25M 4  "1.1"
-    1.25M 8  "1.2"
-    1.25M 10 "1.25"
-    1.25M 12 "1.3"
-    1.25M 16 "1.4"
-    1.25M 3  "1.0202020202"
+  (let [opts {::b/digit-mappings "0123456789abcdef"}]
+    (are [d base expected] (= expected (b/to-base d base opts))
+      1.25M 2  "1.01"
+      1.25M 4  "1.1"
+      1.25M 8  "1.2"
+      1.25M 10 "1.25"
+      1.25M 12 "1.3"
+      1.25M 16 "1.4"
+      1.25M 3  "1.0202020202"
 
-    1.23456M 5  "1.10413"
-    1.23456M 2  "1.0011110000"
-    1.23456M 4  "1.0330003001"
-    1.23456M 8  "1.1700603762"
-    1.23456M 10 "1.23456"
-    1.23456M 12 "1.2993a04a74"
-    1.23456M 16 "1.3c0c1fc8f3"
+      1.23456M 5  "1.10413"
+      1.23456M 2  "1.0011110000"
+      1.23456M 4  "1.0330003001"
+      1.23456M 8  "1.1700603762"
+      1.23456M 10 "1.23456"
+      1.23456M 12 "1.2993a04a74"
+      1.23456M 16 "1.3c0c1fc8f3"
 
-    25.2M 2 "11001.0011001100"
-    25.2M 4   "121.0303030303"
-    25.2M 8    "31.1463146314"
-    25.2M 12   "21.2497249724"
-    25.2M 16   "19.3333333333"
+      25.2M 2 "11001.0011001100"
+      25.2M 4   "121.0303030303"
+      25.2M 8    "31.1463146314"
+      25.2M 12   "21.2497249724"
+      25.2M 16   "19.3333333333"
 
-    1.0M 2 "1.0"
-    1.0M 3 "1.0"
-    1.0M 4 "1.0"
-    1.0M 5 "1.0")
+      1.0M 2 "1.0"
+      1.0M 3 "1.0"
+      1.0M 4 "1.0"
+      1.0M 5 "1.0"))
   (is (apply = (map (partial b/to-base 0.0M) (range 2 37)))
       "0.0₁₀=0.0₂=0.0₃=..."))
 
